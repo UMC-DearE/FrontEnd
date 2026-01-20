54adf2fb95debf0505861866cbe437bd1046e595
riveryunny <gayunphone@gmail.com>
2026-01-21 07:16:58 +0900

    fix: font-normal로 수정

--- src/components/header/SetFromHeader.tsx
+++ src/components/header/SetFromHeader.tsx
@@ -13,7 +13,7 @@ export default function SetFromHeader() {
           <div className="flex items-center space-x-2">
             <button
               type="button"
-              className="text-base font-regular text-[#555557]"
+              className="text-base font-normal text-[#555557]"
               aria-label="관리"
             >
               관리
