7d6f5257f407e9ac042293122dfc7c8490f486d0
kxxnayun <nayun2179@gmail.com>
2026-01-21 10:52:11 +0900

    feat: 편지함 검색창 UI 구현

--- src/components/header/LetterBoxHeader.tsx
+++ src/components/header/LetterBoxHeader.tsx
@@ -1,18 +1,30 @@
 // 편지함 페이지 헤더
 
+import { useState } from 'react';
 import TopSection from './TopSection';
 import type { JSX } from 'react';
 import SearchButton from '@/components/common/header/SearchButton';
+import SearchBar from '../letterBox/SearchBar';
 
 export default function LetterBoxHeader({ title }: { title?: string }): JSX.Element {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
   return (
-    <TopSection
-      left={
-        <div className="flex items-center text-xl font-semibold leading-none text-primary">
-          {title}
+    <div className="relative">
+      <TopSection
+        left={
+          <div className="flex items-center text-xl font-semibold leading-none text-primary">
+            {title}
+          </div>
+        }
+        right={<SearchButton />}
+      />
+
+      {isSearchOpen && (
+        <div className="absolute left-1/2 top-15 -translate-x-1/2">
+          <SearchBar onClose={() => setIsSearchOpen(false)} />
         </div>
-      }
-      right={<SearchButton />}
-    />
+      )}
+    </div>
   );
 }
