eb65a21673581ed7a2ab1c0b3b77edc098e7b596
kxxnayun <nayun2179@gmail.com>
2026-02-09 02:55:05 +0900

    fix: 빌드 오류 수정

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,16 +1,21 @@
-import axios from 'axios';
+import axios, { type AxiosInstance, type InternalAxiosRequestConfig } from 'axios';
 import { getAccessToken } from '@/utils/tempToken';
 
-export const api = axios.create({
+export const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
-api.interceptors.request.use((config) => {
-  const token = getAccessToken();
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = getAccessToken();
 
-  if (token) {
-    config.headers?.set('Authorization', `Bearer ${token}`);
+    if (token && config.headers) {
+      config.headers.set('Authorization', `Bearer ${token}`);
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-
-  return config;
-});
+);
