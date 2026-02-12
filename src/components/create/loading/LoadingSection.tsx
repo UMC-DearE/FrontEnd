3edd118ddca829c0fda8759bb32cebc2d3ff145c
riveryunny <gayunphone@gmail.com>
2026-02-12 19:54:21 +0900

    feat: 수정 화면 로딩 UI 추가

--- src/components/create/loading/LoadingSection.tsx
+++ src/components/create/loading/LoadingSection.tsx
@@ -1,12 +1,27 @@
 import loadingImg from "@/assets/create/loading.svg";
 
-export default function LoadingSection() {
+type LoadingSectionProps = {
+  className?: string;
+  title?: string;
+  subtitle?: string;
+};
+
+export default function LoadingSection({
+  className,
+  title = "편지를 읽고 있어요",
+  subtitle = "reading",
+}: LoadingSectionProps) {
   return (
-    <div className="h-full flex flex-col items-center justify-center gap-[20px] mt-50">
+    <div
+      className={`h-full flex flex-col items-center justify-center gap-[20px] mt-50 ${
+        className ?? ""
+      }`}
+    >
       <img src={loadingImg} alt="loading" className="w-[71px] h-[65px] shake" />
       <div className="flex flex-col items-center">
-      <p className="text-base text-[#555557] font-medium">편지를 읽고 있어요</p>
-      <p className="text-sm font-medium text-[#9D9D9F]">reading</p></div>
+        <p className="text-base text-[#555557] font-medium">{title}</p>
+        <p className="text-sm font-medium text-[#9D9D9F]">{subtitle}</p>
+      </div>
 
       <div className="w-[135px] h-[15px] bg-[#F4F5F6] rounded-[20px] overflow-hidden px-[2px] py-[1px]">
         <div className="loading-grow h-[12px] rounded-[20px]" />
