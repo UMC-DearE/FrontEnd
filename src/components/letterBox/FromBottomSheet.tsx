c62c45c781e1fb9339c1b81491b965195cdc9e17
kxxnayun <nayun2179@gmail.com>
2026-04-08 12:24:52 +0900

    feat: 프롬 바텀 시트 반응형 적용

--- src/components/letterBox/FromBottomSheet.tsx
+++ src/components/letterBox/FromBottomSheet.tsx
@@ -21,9 +21,9 @@ export default function FromBottomSheet({
 }: FromBottomSheetProps) {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <div className="relative w-[393px] min-h-screen overflow-hidden">
+      <div className="relative w-full max-w-[440px] min-h-screen overflow-hidden">
         <button type="button" onClick={onClose} className="absolute inset-0 bg-black/40" />
-        <div className="fixed bottom-0 left-1/2 w-[393px] h-[260px] -translate-x-1/2 rounded-t-[17px] bg-white">
+        <div className="fixed bottom-0 left-1/2 w-full max-w-[440px] h-[260px] -translate-x-1/2 rounded-t-[17px] bg-white">
           <div className="flex h-full flex-col">
             <p className="mt-[21px] text-center text-[18px] font-semibold text-[#141517]">
               From 선택
