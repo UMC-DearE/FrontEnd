bbcb2e5431aa1c76cf84aa7a873630553fa17c8b
riveryunny <gayunphone@gmail.com>
2026-05-20 01:08:03 +0900

    feat: 마진 값 수정

--- src/pages/letter/EditLetterPage.tsx
+++ src/pages/letter/EditLetterPage.tsx
@@ -151,7 +151,7 @@ export default function EditLetterPage() {
       <div
         className="flex-1 px-4"
         style={{
-          marginTop: 'calc(98px + max(env(safe-area-inset-top, 0px) - 20px, 0px))',
+          marginTop: 'calc(98px + max(env(safe-area-inset-top, 0px) - 32px, 0px))',
         }}
       >
         {showSubmittingLoading ? (
